6b64d1424db844351a99366a56af546bf083546d
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-14 20:51:07 -0500

    add initial index.html

---
+++ script.js
@@ -0,0 +1,14 @@
+// Before page loads, check for local storage
+// If local storage found, load local storage
+// On page load, get user location
+// Load users address into default map address
+// Process User Searches
+    // Use EV API to find results
+    // Write Search Results to Page
+        // Display Weather Along With Search Results
+// Change Default Map When User Selects A Location From Search Results
+// Handle click of search button
+// Handle click of individual search results
+// Keep a search history of past results
+// Save the search history to local storage
+
