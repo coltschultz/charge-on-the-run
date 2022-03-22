37b6795df9a09391fdc52e20be6f0438e3771fb4
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-21 21:32:59 -0500

    use current location button, apply prettier

--- assets/js/coltscript.js
+++ assets/js/coltscript.js
@@ -1,11 +1,11 @@
 var LAT;
 var LON;
-var currentCity = '';
+var currentCity = "";
 var prevInfo = false;
 var currentPosition = document.getElementById("demo");
-const searchBar = document.getElementById('searchBar');
+const searchBar = document.getElementById("searchBar");
 function getData(city) {
-
+  event.preventDefault();
   var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
   var apiKey = "e9da07741ba3933502e8f95cfbb33359";
 
@@ -13,10 +13,11 @@ function getData(city) {
 
   currentCity = city;
   currentCity = currentCity.toUpperCase();
+
   if (!historyArray.includes(currentCity)) {
     logSearch();
   }
-  
+
   fetch(url)
     .then(function (response) {
       return response.json();
@@ -28,40 +29,40 @@ function getData(city) {
     });
 }
 
-// codys function 
+// codys function
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        currentPosition.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    currentPosition.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
-var longitude = '';
-var latitude = '';
-function showPosition(position) { 
-    console.log(position.coords)
-    // change position.coords.latitude to varible 
-    // currentPosition.innerHTML = "Latitude: " + position.coords.latitude +
-    //     "<br>Longitude: " + position.coords.longitude;
-        longitude = position.coords.longitude;
-        latitude = position.coords.latitude;
+var longitude = "";
+var latitude = "";
+function showPosition(position) {
+  console.log(position.coords);
+  // change position.coords.latitude to varible
+  // currentPosition.innerHTML = "Latitude: " + position.coords.latitude +
+  //     "<br>Longitude: " + position.coords.longitude;
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude;
 }
 
 function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            currentPosition.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            currentPosition.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            currentPosition.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            currentPosition.innerHTML = "An unknown error occurred."
-            break;
-    }
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      currentPosition.innerHTML = "User denied the request for Geolocation.";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      currentPosition.innerHTML = "Location information is unavailable.";
+      break;
+    case error.TIMEOUT:
+      currentPosition.innerHTML = "The request to get user location timed out.";
+      break;
+    case error.UNKNOWN_ERROR:
+      currentPosition.innerHTML = "An unknown error occurred.";
+      break;
+  }
 }
 getLocation();
 
@@ -85,7 +86,11 @@ function updateMap(LAT, LON) {
 
   // Perform a nearby search.
   places.nearbySearch(
-    { location: currentLocation, radius: 50000, keyword: "EV charging station" },
+    {
+      location: currentLocation,
+      radius: 50000,
+      keyword: "EV charging station",
+    },
     (results, status, pagination) => {
       if (status !== "OK" || !results) return;
 
@@ -105,9 +110,9 @@ function updateMap(LAT, LON) {
 function addPlaces(places, map) {
   const placesList = document.getElementById("places");
 
-  // added if" to update results list with newest search .bycody 
-  if (placesList.children.length > 0){
-    placesList.innerHTML = '';
+  // added if" to update results list with newest search .bycody
+  if (placesList.children.length > 0) {
+    placesList.innerHTML = "";
   }
 
   for (const place of places) {
@@ -128,9 +133,8 @@ function addPlaces(places, map) {
       });
 
       const li = document.createElement("li");
-      const searchButton = document.getElementById('searchButton');
+      const searchButton = document.getElementById("searchButton");
 
-      
       var request = {
         placeId: place.place_id,
         fields: [
@@ -174,8 +178,8 @@ function addPlaces(places, map) {
             });
           });
 
-          
           li.addEventListener("click", () => {
+            document.getElementById("map").scrollIntoView();
             map.setCenter(place.geometry.location);
             if (prevInfo) {
               prevInfo.close();
@@ -194,51 +198,48 @@ function addPlaces(places, map) {
 }
 
 // Local Storage Handling
-var historyListEl = document.getElementById('historyListEl');
+var historyListEl = document.getElementById("historyListEl");
 var historyArray = [];
-var searchDataObj = '';
+var searchDataObj = "";
 
 // Load the prior search history when the page loads
-(function(item) {
-historyArray = JSON.parse(localStorage.getItem('historyArray'));
-if (historyArray) {
+(function (item) {
+  historyArray = JSON.parse(localStorage.getItem("historyArray"));
+  if (historyArray) {
     historyArray.splice(10);
-    
+
     for (var i = 0; i < historyArray.length; i++) {
-    var newEl = document.createElement("li")
-    newEl.textContent = historyArray[i];
-    historyListEl.appendChild(newEl);
+      var newEl = document.createElement("li");
+      newEl.textContent = historyArray[i];
+      historyListEl.appendChild(newEl);
     }
-}
-else {
-    var decoy = '';
+  } else {
+    var decoy = "";
     historyArray = [decoy];
-}
-})()
+  }
+})();
 
 // Log new searches into the history
-var logSearch = function() {
+var logSearch = function () {
   cityUpper = currentCity.toUpperCase();
   searchDataObj = cityUpper;
   if (historyArray) {
-  historyArray.unshift(searchDataObj);
-  }
-  else {
-      historyArray.push(searchDataObj);
+    historyArray.unshift(searchDataObj);
+  } else {
+    historyArray.push(searchDataObj);
   }
   historyArray.splice(10);
-  localStorage.setItem('historyArray', JSON.stringify(historyArray));
+  localStorage.setItem("historyArray", JSON.stringify(historyArray));
   var newEl = document.createElement("li");
   newEl.textContent = cityUpper;
   historyListEl.insertBefore(newEl, historyListEl.firstChild);
-  
-}
+};
 
-searchButton.addEventListener("click", 
-function(event){
+searchButton.addEventListener("click", function (event) {
   event.preventDefault();
-  historyListEl.innerHTML = "";
-  getData(searchBar.value)});
+  places.innerHTML = "";
+  getData(searchBar.value);
+});
 
 // Handle clicks of history buttons
 $("#historyListEl").on("click", "li", function () {
@@ -250,11 +251,11 @@ $("#historyListEl").on("click", "li", function () {
 });
 
 // Handle Click of Use Current Location Button
-var currentLocationEl = document.getElementById('currentLocation');
+var currentLocationEl = document.getElementById("currentLocation");
 
-currentLocationEl.addEventListener("click", function() {
+currentLocationEl.addEventListener("click", function () {
   updateMap(latitude, longitude);
-})
+});
 
 // Make so it scrolls to map on click of result
 // Make sure results show on map on click of history
